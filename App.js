656d0f5984d217612c3cf127dd3e299fe0e17dad
Jacquelimemunoz <munozjac3508@gmail.com>
2025-03-24 19:33:18 -0500

    Update App.js

--- App.js
+++ App.js
@@ -1,11 +1,1 @@
-import NavBar from "./components/NavBar";
-import ItemListContainer from "./components/ItemListContainer";
 
-function App() {
-  return (
-    <>
-      <NavBar />
-      <ItemListContainer greeting="¡Bienvenido a nuestra tienda!" />
-    </>
-  );
-}
